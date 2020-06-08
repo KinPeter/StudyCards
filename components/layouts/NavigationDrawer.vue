5fa7183f858a8502f80a9fb3b4057d18277b7a58
Peter Kin <kinpeter85@gmail.com>
2020-06-08 08:53:23 +0200

    Put routes and actions to nav drawer

--- components/layouts/NavigationDrawer.vue
+++ components/layouts/NavigationDrawer.vue
@@ -1,29 +1,31 @@
 <template>
   <v-navigation-drawer :value="open" @input="onChange" fixed app>
     <v-list>
-      <v-list-item
-        v-for="(item, i) in routes"
-        :key="i"
-        :to="item.to"
-        router
-        exact
-      >
+      <v-list-item v-if="routes[0].show" :to="routes[0].to" router exact>
         <v-list-item-action>
-          <v-icon>{{ item.icon }}</v-icon>
+          <v-icon>{{ routes[0].icon }}</v-icon>
         </v-list-item-action>
         <v-list-item-content>
-          <v-list-item-title v-text="item.title" />
+          <v-list-item-title v-text="routes[0].title" />
         </v-list-item-content>
       </v-list-item>
     </v-list>
     <v-divider></v-divider>
     <v-list>
-      <v-list-item v-for="(item, i) in actions" :key="i" @click="item.action">
+      <v-list-item v-if="actions[0].show" @click="actions[0].action">
         <v-list-item-action>
-          <v-icon>{{ item.icon }}</v-icon>
+          <v-icon>{{ actions[0].icon }}</v-icon>
         </v-list-item-action>
         <v-list-item-content>
-          <v-list-item-title v-text="item.title" />
+          <v-list-item-title v-text="actions[0].title" />
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item v-if="actions[1].show" @click="actions[1].action">
+        <v-list-item-action>
+          <v-icon>{{ actions[1].icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title v-text="actions[1].title" />
         </v-list-item-content>
       </v-list-item>
     </v-list>
@@ -31,35 +33,60 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, SetupContext } from '@vue/composition-api'
+import {
+  computed,
+  defineComponent,
+  ref,
+  Ref,
+  SetupContext,
+} from '@vue/composition-api'
 
 interface Props {
-  routes: []
-  actions: []
   open: boolean
 }
 
 export default defineComponent({
   props: {
-    routes: {
-      type: Array,
-      required: true,
-    },
-    actions: {
-      type: Array,
-      required: true,
-    },
     open: {
       type: Boolean,
       required: true,
     },
   },
   setup(_props: Props, ctx: SetupContext) {
+    const routes: Ref<[]> = ref([
+      {
+        icon: 'mdi-chart-bubble',
+        title: 'Inspire',
+        to: '/inspire',
+        show: true,
+      },
+    ])
+    const actions: Ref<[]> = ref([
+      {
+        icon: 'mdi-theme-light-dark',
+        title: 'Theme',
+        action: () => {
+          ctx.root.$vuetify.theme.dark = !ctx.root.$vuetify.theme.dark
+        },
+        show: true,
+      },
+      {
+        icon: 'mdi-logout-variant',
+        title: 'Log out',
+        action: () => {
+          ctx.root.$auth.logout()
+        },
+        show: () => ctx.root.$auth.loggedIn, // FIXME
+      },
+    ])
+
     const onChange = (opened: boolean) => {
       ctx.emit('update:open', opened)
     }
     return {
       onChange,
+      routes,
+      actions,
     }
   },
 })
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app dark>
-    <NavigationDrawer :open.sync="drawer" :routes="routes" :actions="actions" />
+    <NavigationDrawer :open.sync="drawer" />
     <AppBar :drawer-open="drawer" @burgerClick="toggleDrawer" />
     <v-content>
       <v-container>
@@ -20,45 +20,16 @@ export default defineComponent({
     NavigationDrawer,
     AppBar,
   },
-  setup(_props, ctx: SetupContext) {
+  setup(_props, _ctx: SetupContext) {
     const drawer: Ref<boolean> = ref(false)
 
     const toggleDrawer = (): void => {
       drawer.value = !drawer.value
     }
 
-    const initRoutes = [
-      {
-        icon: 'mdi-chart-bubble',
-        title: 'Inspire',
-        to: '/inspire',
-      },
-    ]
-    const initActions = [
-      {
-        icon: 'mdi-theme-light-dark',
-        title: 'Theme',
-        action: () => {
-          ctx.root.$vuetify.theme.dark = !ctx.root.$vuetify.theme.dark
-        },
-      },
-      {
-        icon: 'mdi-logout-variant',
-        title: 'Log out',
-        action: () => {
-          ctx.root.$auth.logout()
-        },
-      },
-    ]
-
-    const routes: Ref<[]> = ref(initRoutes)
-    const actions: Ref<[]> = ref(initActions)
-
     return {
       drawer,
       toggleDrawer,
-      routes,
-      actions,
     }
   },
 })
